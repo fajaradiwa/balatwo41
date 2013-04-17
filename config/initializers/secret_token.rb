50e73abcf1bab99ef4a6da9bc327990515564972
fajaradiwa <fajaradiwa@fajaradiwa.(none)>
2013-04-17 10:15:31 +0700

    add app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blekedek41::Application.config.secret_token = '9747633dc0df1e369ae859f870b96559a959b0885f94cf60c31ee853bbad4383754c6ce2c172666228feb328e4b7f8ed7834abefbfb03722b97c32ed1df9fd19'
